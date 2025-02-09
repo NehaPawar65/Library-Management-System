3af4abd367342b92a7ae2cb143fd0b685e196a5d
NehaPawar65 <nehabpawar0605@gmail.com>
2025-02-09 17:39:47 +0530

    Updated

--- Main.java
+++ Main.java
@@ -1,48 +1,56 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) 
+    {
         Library library = new Library();
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        while (true) 
+        {
             System.out.println("\nLibrary Management System");
-            System.out.println("1. Add Book");
-            System.out.println("2. Issue Book");
+            System.out.println("1. Add Book in the Library");
+            System.out.println("2. Issue Book from the Library");
             System.out.println("3. Return Book");
             System.out.println("4. Display Books");
             System.out.println("5. Exit");
             System.out.print("Choose an option: ");
             
             int choice = scanner.nextInt();
-            switch (choice) {
+            switch (choice) 
+            {
                 case 1:
                     System.out.print("Enter Book ID: ");
                     int id = scanner.nextInt();
-                    scanner.nextLine();  // Consume newline
                     System.out.print("Enter Book Title: ");
                     String title = scanner.nextLine();
                     System.out.print("Enter Author: ");
                     String author = scanner.nextLine();
                     library.addBook(new Book(id, title, author));
                     break;
+
                 case 2:
                     System.out.print("Enter Book ID to Issue: ");
                     int issueId = scanner.nextInt();
                     library.issueBook(issueId);
                     break;
+
                 case 3:
                     System.out.print("Enter Book ID to Return: ");
                     int returnId = scanner.nextInt();
                     library.returnBook(returnId);
                     break;
+
                 case 4:
                     library.displayBooks();
                     break;
+
                 case 5:
                     System.out.println("Exiting Library System...");
                     scanner.close();
                     return;
+
                 default:
                     System.out.println("Invalid choice, try again!");
             }
