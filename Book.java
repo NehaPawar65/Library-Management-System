6a75587009a818fd02d2b9becb5cb57b9e6913a0
NehaPawar65 <nehabpawar0605@gmail.com>
2025-02-09 17:48:50 +0530

    Completed

--- Book.java
+++ Book.java
@@ -1,26 +1,47 @@
-public class Book {
+public class Book
+ {
     private int id;
     private String title;
     private String author;
     private boolean isIssued;
 
-    public Book(int id, String title, String author) {
+    public Book(int id, String title, String author) 
+    {
         this.id = id;
         this.title = title;
         this.author = author;
         this.isIssued = false;
     }
 
-    public int getId() { return id; }
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public boolean isIssued() { return isIssued; }
+    public int getId()
+    {
+        return id;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getAuthor()
+    {
+        return author;
+    }
+    public boolean isIssued()
+    {
+        return isIssued;
+    }
     
-    public void issueBook() { this.isIssued = true; }
-    public void returnBook() { this.isIssued = false; }
+    public void issueBook()
+    {
+        this.isIssued = true;
+    }
+    public void returnBook()
+    {
+        this.isIssued = false;
+    }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Book ID: " + id + ", Title: " + title + ", Author: " + author + ", Issued: " + isIssued;
     }
 }
